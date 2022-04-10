b4f4547224b2d61842a954c7e0c148ba512ca868
Marmik Patel <marmikpatel250@gmail.com>
2022-04-10 19:23:44 +0530

    bugfix

--- taskban_backend/users/api/views.py
+++ taskban_backend/users/api/views.py
@@ -34,6 +34,7 @@ class ExcludeBoardMembersFilter(filters.BaseFilterBackend):
 
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
+    permissions_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
