6be9733ab3d9a3ca09c28c85cd85988ed06c021f
Marmik Patel <marmikpatel250@gmail.com>
2022-04-14 21:48:49 +0530

    added name attribute in user registration form.

--- taskban_backend/users/api/serializers.py
+++ taskban_backend/users/api/serializers.py
@@ -72,7 +72,7 @@ class BoardMemberSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'name', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
